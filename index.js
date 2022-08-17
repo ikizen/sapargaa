61187438ef3d877e574c1660afc69cad3f1e1eca
ikizen <aisaitrash@gmail.com>
2022-08-17 17:10:12 +0600

    parsing changes: checking on railway.app

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ import { connectToCluster } from "./db.js";
 
 const app = express();
 const port = 8080;
-const urlHotel =
-    "https://www.tripadvisor.com/Hotels-g298251-Almaty-Hotels.html";
+// const urlHotel =
+// "https://www.tripadvisor.com/Hotels-g298251-Almaty-Hotels.html";
 
 const urlTaxi = "https://www.numbeo.com/taxi-fare/in/Almaty";
 
-const urlAnytime = "https://anytime.kz/rates.html";
+// const urlAnytime = "https://anytime.kz/rates.html";
 
 // FETCH AND SAVE TO DB EVERY 10 MINUTES
 cron.schedule("* */1 * * * *", function () {
@@ -36,84 +36,85 @@ cron.schedule("* */1 * * * *", function () {
 
             const anytimeCollection = db.collection("anytime");
             //Parsing HOTEL
-            await axios(urlHotel)
-                .then(async (response) => {
-                    const html = response.data;
-                    const $ = cheer.load(html);
-                    const array = [];
-                    const sepNov = $(".page")
-                        .children(".delineation")
-                        .children("#MAINWRAP")
-                        .children("#MAIN")
-                        .children("#BODYCON")
-                        .children(".bodycon_main")
-                        .children(".relWrap")
-                        .children("#taplc_cupid_pricing_trends_0")
-                        .children("#component_5")
-                        .find("div")
-                        .first()
-                        .children(".ui_section")
-                        .children("div:nth-child(7)")
-                        .children(".OPtaC")
-                        .children(".cmvHE")
-                        .children(".orWuf")
-                        .children(".HxUSW")
-                        .children("div:nth-child(2)")
-                        .html();
+            // await axios(urlHotel)
+            //     .then(async (response) => {
+            //         const html = response.data;
+            //         const $ = cheer.load(html);
+            //         const array = [];
+            //         const sepNov = $(".page")
+            //             .children(".delineation")
+            //             .children("#MAINWRAP")
+            //             .children("#MAIN")
+            //             .children("#BODYCON")
+            //             .children(".bodycon_main")
+            //             .children(".relWrap")
+            //             .children("#taplc_cupid_pricing_trends_0")
+            //             .children("#component_5")
+            //             .find("div")
+            //             .first()
+            //             .children(".ui_section")
+            //             .children("div:nth-child(7)")
+            //             .children(".OPtaC")
+            //             .children(".cmvHE")
+            //             .children(".orWuf")
+            //             .children(".HxUSW")
+            //             .children("div:nth-child(2)")
+            //             .html();
 
-                    const marMay = $(".page")
-                        .children(".delineation")
-                        .children("#MAINWRAP")
-                        .children("#MAIN")
-                        .children("#BODYCON")
-                        .children(".bodycon_main")
-                        .children(".relWrap")
-                        .children("#taplc_cupid_pricing_trends_0")
-                        .children("#component_5")
-                        .find("div")
-                        .first()
-                        .children(".ui_section")
-                        .children(".MqYSb")
-                        .children(".OPtaC")
-                        .children(".cmvHE")
-                        .children(".orWuf")
-                        .children(".HxUSW")
-                        .children("div:nth-child(2)")
-                        .html();
+            //         const marMay = $(".page")
+            //             .children(".delineation")
+            //             .children("#MAINWRAP")
+            //             .children("#MAIN")
+            //             .children("#BODYCON")
+            //             .children(".bodycon_main")
+            //             .children(".relWrap")
+            //             .children("#taplc_cupid_pricing_trends_0")
+            //             .children("#component_5")
+            //             .find("div")
+            //             .first()
+            //             .children(".ui_section")
+            //             .children(".MqYSb")
+            //             .children(".OPtaC")
+            //             .children(".cmvHE")
+            //             .children(".orWuf")
+            //             .children(".HxUSW")
+            //             .children("div:nth-child(2)")
+            //             .html();
 
-                    const reg = /\d+/g;
-                    let result1 = sepNov.match(reg);
-                    let result2 = marMay.match(reg);
-                    const resultSepNov = result1[0] + result1[1];
-                    const resultMarMay = result2[0] + result2[1];
-                    array.push(resultSepNov, resultMarMay);
+            //         const reg = /\d+/g;
+            //         let result1 = sepNov.match(reg);
+            //         let result2 = marMay.match(reg);
+            //         const resultSepNov = result1[0] + result1[1];
+            //         const resultMarMay = result2[0] + result2[1];
+            //         array.push(resultSepNov, resultMarMay);
 
-                    const hotelCount =
-                        (parseInt(resultMarMay) + parseInt(resultSepNov)) / 2;
+            //         const hotelCount =
+            //             (parseInt(resultMarMay) + parseInt(resultSepNov)) / 2;
 
-                    console.log(hotelCount);
+            //         console.log(hotelCount);
 
-                    // If hotel count exist - update it. If not - create it.
-                    await hotelCollection.replaceOne(
-                        { dataType: "count" },
-                        { count: hotelCount },
-                        {
-                            upsert: true,
-                        }
-                    );
-                })
-                .catch((err) => console.log(`Error in hotel part:`, err));
+            //         // If hotel count exist - update it. If not - create it.
+            //         await hotelCollection.replaceOne(
+            //             { dataType: "count" },
+            //             { count: hotelCount },
+            //             {
+            //                 upsert: true,
+            //             }
+            //         );
+            //     })
+            //     .catch((err) => console.log(`Error in hotel part:`, err));
 
             //Parsing TAXI
             await axios(urlTaxi)
                 .then(async (response) => {
                     const html = response.data;
                     const $ = cheer.load(html);
-                    const taxi = $(".innerWidth")
+                    const taxi = $("body")
+                        .children(".innerWidth")
                         .children(".standard_margin")
                         .first()
                         .find("tbody")
-                        .children(".tr_standard")
+                        .children(".tr_highlighted")
                         .children("td:nth-child(2)")
                         .html();
                     // console.log(taxi);
@@ -135,44 +136,6 @@ cron.schedule("* */1 * * * *", function () {
                     console.log("ERROR in taxi part:", err);
                 });
 
-            //Parsing Anytime
-            // await axios(urlAnytime)
-            //     .then(async (response) => {
-            //         const html = response.data;
-            //         const $ = cheer.load(html);
-            //         const anytime = $(".body_wrap")
-            //             // .children(".section:nth-child(4)")
-            //             // .first()
-            //             // .first()
-            //             // .first()
-            //             // .children("div:nth-child(15)")
-            //             // .children("div:nth-child(2)")
-            //             // .first()
-
-            //             // .children(".container")
-            //             // .children(".section__wrap")
-            //             // .children(".cards")
-            //             // .children(".rates-card")
-            //             // .children(".rates-card__items")
-            //             // .children(".item:nth-child(2)")
-            //             // .children("div:nth-child(2)")
-            //             .html();
-            //         console.log(anytime);
-            //         // const reg = /\d+/g;
-            //         // let resultAnytime = anytime.match(reg);
-            //         // const anytimeCount = parseInt(resultAnytime);
-            //         // //         // console.log(anytimeCount);
-
-            //         // await anytimeCollection.replaceOne(
-            //         //     {},
-            //         //     { count: anytimeCount },
-            //         //     { upsert: true }
-            //         // );
-            //     })
-            //     .catch((err) => {
-            //         console.log("ERROR in anytime part:", err);
-            //     });
-
             // PLACES TRIPADVISERY
 
             mongoClient.close();
@@ -185,32 +148,28 @@ cron.schedule("* */1 * * * *", function () {
 
 app.use(cors());
 
-app.get("/", (res, req) => {
-    console.log("lohgfhfgh");
-});
+// app.get("/hotel", (req, res) => {
+//     let mongoClient;
 
-app.get("/hotel", (req, res) => {
-    let mongoClient;
+//     // Get access to mongo db cluster
+//     connectToCluster()
+//         .then(async (client) => {
+//             mongoClient = client;
 
-    // Get access to mongo db cluster
-    connectToCluster()
-        .then(async (client) => {
-            mongoClient = client;
+//             // Connect to DB named "travel-expense"
+//             const db = client.db("travel-expense");
 
-            // Connect to DB named "travel-expense"
-            const db = client.db("travel-expense");
+//             // Get collection in the db named "hotel"
+//             const hotelCollection = db.collection("hotel");
 
-            // Get collection in the db named "hotel"
-            const hotelCollection = db.collection("hotel");
+//             // Find first item in that collection
+//             const hotel = await hotelCollection.findOne();
 
-            // Find first item in that collection
-            const hotel = await hotelCollection.findOne();
-
-            // Return it's count (count is where we saved the number of hotels)
-            res.status(200).json(hotel.count);
-        })
-        .finally(() => mongoClient.close());
-});
+//             // Return it's count (count is where we saved the number of hotels)
+//             res.status(200).json(hotel.count);
+//         })
+//         .finally(() => mongoClient.close());
+// });
 
 app.get("/transport", (req, res) => {
     let mongoClient;
@@ -234,28 +193,5 @@ app.get("/transport", (req, res) => {
         })
         .finally(() => mongoClient.close());
 });
-
-// app.get("/anytime", (req, res) => {
-//     let mongoClient;
-
-//     // Get access to mongo db cluster
-//     connectToCluster()
-//         .then(async (client) => {
-//             mongoClient = client;
-
-//             // Connect to DB named "travel-expense"
-//             const db = client.db("travel-expense");
-
-//             // Get collection in the db named "transport"
-//             const anytimeCollection = db.collection("anytime");
-
-//             // Find first item in that collection
-//             const anytime = await anytimeCollection.findOne();
-
-//             // Return it's count (count is where we saved the number of transports)
-//             res.status(200).json(anytime.count);
-//         })
-//         .finally(() => mongoClient.close());
-// });
 
 app.listen(port, () => console.log(`Server is working on PORT ${port}`));
